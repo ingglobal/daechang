0487980bcbf0e11fade5100f535331a8f6dd2ffd
ingglobal <rnd@ingglobal.net>
2023-10-30 16:03:01 +0900

    tmd pg_socket_list

--- adm/v10/db/_top_menu_data.php
+++ adm/v10/db/_top_menu_data.php
@@ -9,12 +9,19 @@ if($member['mb_level']>=9) {
     // $sub_title_list = ' <a href="'.G5_BBS_URL.'/board.php?bo_table=setting1" class="btn_top_menu '.$active_term_list.'">환경설정게시판</a>
     // ';
 }
-
+/*
 $g5['container_sub_title'] = '
 <h2 id="container_sub_title">
     <a href="./pg_socket_list.php" class="btn_top_menu '.$active_pg_socket_list.'">소켓데이터</a>
     <a href="./my_data_run_list.php" class="btn_top_menu '.$active_my_data_run_list.'">설비별가동</a>
     <a href="./my_data_error_list.php" class="btn_top_menu '.$active_my_data_error_list.'">알람발생</a>
+	'.$sub_title_list.'
+</h2>
+';
+*/
+$g5['container_sub_title'] = '
+<h2 id="container_sub_title">
+    <a href="./pg_socket_list.php" class="btn_top_menu '.$active_pg_socket_list.'">소켓데이터</a>
 	'.$sub_title_list.'
 </h2>
 ';
