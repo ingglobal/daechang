6d0eb7e0ce56ac1a5f1a4256639f8f1406d16a75
ingglobal <rnd@ingglobal.net>
2023-11-23 18:11:55 +0900

    worker num2

--- adm/v10/worker_list_update.php
+++ adm/v10/worker_list_update.php
@@ -33,10 +33,6 @@ if ($_POST['act_button'] == "선택수정") {
 
         if (!$mb['mb_id']) {
             $msg .= $mb['mb_id'].' : 회원자료가 존재하지 않습니다.\\n';
-        } else if ($is_admin != 'super' && $mb['mb_level'] >= $member['mb_level']) {
-            $msg .= $mb['mb_id'].' : 자신보다 권한이 높거나 같은 회원은 수정할 수 없습니다.\\n';
-        } else if ($member['mb_id'] == $mb['mb_id']) {
-            $msg .= $mb['mb_id'].' : 로그인 중인 관리자는 수정 할 수 없습니다.\\n';
         } else {
             
             $sql = "UPDATE {$g5['member_table']} SET
