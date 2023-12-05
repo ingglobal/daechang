0f06a9a1e1f3775a6337e8aba8884ca94049d040
ingglobal <rnd@ingglobal.net>
2023-12-05 17:20:26 +0900

    some change applied

--- user/cron/cpu_check_restart.sh
+++ user/cron/cpu_check_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 로그파일경로
-log_file="/home/daechang/www/user/log/cpu_check_log.txt"
+log_file="../log/cpu_check_log.txt"
 
 # CPU 사용량 확인
 cpu_usage_php=$(top -bn1 | grep "php-fpm7\.4" | head -n 1 | awk '{print $9}')
@@ -14,12 +14,15 @@ cpu_percentage_mysql=${cpu_usage_mysql%.*}
 cpu_percentage_postgres=${cpu_usage_postgres%.*}
 
 
-# 로그 데이터 기록 함수
+# 로그 데이터 기록 함수./
 write_log() {
     local message=$1
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$timestamp - $message" >> $log_file
+    # echo "$timestamp - $message" >> $log_file
 }
+top_15=$(top -bn1 | head -n 12)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo -e "$timestamp ---------------------------------------------------------\n$top_15" >> $log_file
 
 # 로그 데이터 기록
 write_log "php-fpm7.4 CPU 사용량: $cpu_usage_php"
