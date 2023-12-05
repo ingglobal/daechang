bfdf9563f56ac056b4b1105d96078f2b2ede52c4
ingglobal <rnd@ingglobal.net>
2023-12-05 18:58:52 +0900

    cpu merge

--- user/cron/cpu_check_restart.sh
+++ user/cron/cpu_check_restart.sh
@@ -2,6 +2,7 @@
 
 # 로그파일경로
 log_file="/home/daechang/www/user/log/cpu_check_log.txt"
+# log_file="../log/cpu_check_log.txt"
 
 # CPU 사용량 확인
 cpu_usage_php=$(top -bn1 | grep "php-fpm7\.4" | head -n 1 | awk '{print $9}')
@@ -18,8 +19,12 @@ cpu_percentage_postgres=${cpu_usage_postgres%.*}
 write_log() {
     local message=$1
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$timestamp - $message" >> $log_file
+    # echo "$timestamp - $message" >> $log_file
 }
+top_15=$(top -bn1 | head -n 15)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo -e "$timestamp ---------------------------------------------------------\n$top_15" >> $log_file
+
 
 # 로그 데이터 기록
 write_log "php-fpm7.4 CPU 사용량: $cpu_usage_php"
@@ -40,7 +45,7 @@ if [ $cpu_percentage_postgres -ge 100 ]; then
 fi
 
 # 로그 파일 최근 1000줄만 남기고 오래된 행은 삭제
-if [ $(wc -l < "$log_file") -gt 1000 ]; then
+if [ $(wc -l < "$log_file") -gt 10000 ]; then
     tail -n 1000 "$log_file" > "$log_file.tmp"
     mv "$log_file.tmp" "$log_file"
 fi
